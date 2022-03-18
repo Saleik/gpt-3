686708122ab216ae5564d9f8028a7bbfebd9a547
Saleik <sarrazinkevin@free.fr>
2022-03-18 15:26:59 +0100

    brands done - WIP whatGP3section

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './feature.css';
+import { Props } from './types/types';
 
-const Feature = () => {
-	return <div>Feature</div>;
+const Feature = ({ title, text }: Props) => {
+	return (
+		<div className='gpt3__features-container__feature'>
+			<div className='gpt3__features-container__feature-title'>
+				<div />
+				<h1>{title}</h1>
+			</div>
+			<div className='gpt3__features-container_feature-text'>{text}</div>
+		</div>
+	);
 };
 
 export default Feature;
